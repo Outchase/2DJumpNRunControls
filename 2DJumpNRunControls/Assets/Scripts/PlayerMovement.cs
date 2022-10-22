7be8c681b0b630547a92ebef7741153fac660b95
Outchase <101480664+Outchase@users.noreply.github.com>
2022-10-22 05:55:25 +0200

    Double Jump and dash added

--- 2DJumpNRunControls/Assets/Scripts/PlayerMovement.cs
+++ 2DJumpNRunControls/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,13 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 400f;
-    public float jumpForce = 10f;
     private Vector2 movementDirection;
     private Rigidbody2D rb;
     private bool isGround;
 
+    public float speed = 400f;
+    public float jumpForce = 17f;
+    public int amountOfJumps;
     public Transform groundCenter;
     public LayerMask groundLayer;
 
@@ -20,27 +21,60 @@ public class PlayerMovement : MonoBehaviour
         movementDirection = context.ReadValue<Vector2>();
     }
 
-    private void Update()
+    public void Update()
+    {
+        isGround = Physics2D.OverlapCircle(groundCenter.position, .2f, groundLayer);
+    }
+
+    private void FixedUpdate()
     {
         rb.velocity = new Vector2(movementDirection.x * speed * Time.deltaTime, rb.velocity.y);
-        isGround = Physics2D.OverlapCircle(groundCenter.position, .2f, groundLayer);
     }
 
     public void Awake()
     {
-        //groundCenter = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    public void Jump(InputAction.CallbackContext context) {
-
-        Debug.Log(isGround);
+    public void Jump(InputAction.CallbackContext context)
+    {
         if (isGround)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumps = 3;
 
+        }
+
+        if (amountOfJumps >= 0)
+        {
+            
+            if (context.performed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+
+            if (context.canceled && rb.velocity.y > 0f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.7f);
+            }
+            amountOfJumps--;
         }
     }
 
+    public void Dash(InputAction.CallbackContext context)
+    {
+        if (isGround)
+        {
+            if (context.performed)
+            {
+                speed = speed *2;
+            }
+        }
 
+        if (context.canceled)
+        {
+            speed = 400f;
+
+        }
+
+    }
 }
